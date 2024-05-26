58abce1118cb9a42f414472275fd68b6161f6c45
Adeeb Abubacker <148857537+adeebca@users.noreply.github.com>
2024-05-26 07:29:40 +0530

    Made some changes

--- controllers/groupController/groupController.js
+++ controllers/groupController/groupController.js
@@ -6,17 +6,14 @@ const User = require('../../models/user.js');
 const createGroup = async (req, res) => {
   try {
     let groupData = req.body;
-    // Check if users array is provided in the request body
     if (groupData.users && Array.isArray(groupData.users)) {
-      // Fetch users by IDs from the database
       const users = await User.find({ _id: { $in: groupData.users } });
-      // Check if all provided user IDs are valid
       if (users.length !== groupData.users.length) {
         return res.status(400).json({ error: 'One or more user IDs are invalid' });
       }
     }
 
-    // Create a new group with the provided data
+
     const group = new Group(groupData);
     await group.save();
     res.status(201).json(group);
@@ -32,21 +29,21 @@ const addUsersToGroup = async (req, res) => {
       return res.status(404).json({ error: 'Group not found' });
     }
 
-    const userIds = req.body.userIds; 
+    const usersToAdd = req.body.users; 
 
-    // Validate user IDs
-    if (!Array.isArray(userIds)) {
-      return res.status(400).json({ error: 'User IDs must be provided as an array' });
+  
+    if (!Array.isArray(usersToAdd) || usersToAdd.length === 0) {
+      return res.status(400).json({ error: 'Users must be provided as a non-empty array' });
     }
 
-    // Fetch users by IDs
-    const users = await User.find({ _id: { $in: userIds } });
+    
+    const existingUsers = await User.find({ email: { $in: usersToAdd.map(user => user.email) } });
 
-    // Add users to the group
-    group.users.push(...userIds);
+
+    group.users.push(...existingUsers.map(user => user._id));
     await group.save();
 
-    res.status(200).json({ message: 'Users added to group', users });
+    res.status(200).json({ message: 'Users added to group', users: existingUsers });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
