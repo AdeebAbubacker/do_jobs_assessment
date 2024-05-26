d81252e05314342ffc0ee2072716b0aa3ad45cd5
Adeeb Abubacker <148857537+adeebca@users.noreply.github.com>
2024-05-26 18:05:37 +0530

    Made some changes

--- controllers/groupController/groupController.js
+++ controllers/groupController/groupController.js
@@ -1,14 +1,18 @@
 const Group = require('../../models/group.js');
 const User = require('../../models/user.js');
-
+const mongoose = require('mongoose');
 
 
 const createGroup = async (req, res) => {
   try {
     let groupData = req.body;
     if (groupData.users && Array.isArray(groupData.users)) {
-      const users = await User.find({ _id: { $in: groupData.users } });
-      if (users.length !== groupData.users.length) {
+      const userAggregation = await User.aggregate([
+        { $match: { _id: { $in: groupData.users.map(userId => new mongoose.Types.ObjectId(userId)) } } },
+        { $group: { _id: null, count: { $sum: 1 } } }
+      ]);
+      
+      if (userAggregation.length === 0 || userAggregation[0].count !== groupData.users.length) {
         return res.status(400).json({ error: 'One or more user IDs are invalid' });
       }
     }
@@ -17,7 +21,11 @@ const createGroup = async (req, res) => {
     await group.save();
     res.status(201).json(group);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    if (error.keyPattern && error.keyPattern.name) {
+      return res.status(400).json({ error: 'Group already exists' });
+    } else {
+      res.status(400).json({ error: error.message });
+    }
   }
 };
 
@@ -31,10 +39,21 @@ const addUsersToGroup = async (req, res) => {
     if (!Array.isArray(usersToAdd) || usersToAdd.length === 0) {
       return res.status(400).json({ error: 'Users must be provided as a non-empty array' });
     }
-    const existingUsers = await User.find({ email: { $in: usersToAdd.map(user => user.email) } });
-    group.users.push(...existingUsers.map(user => user._id));
+
+    const existingUsersAggregation = await User.aggregate([
+      { $match: { email: { $in: usersToAdd.map(user => user.email) } } },
+      { $project: { _id: 1 } }
+    ]);
+
+    const existingUserIds = existingUsersAggregation.map(user => user._id);
+    group.users.push(...existingUserIds);
     await group.save();
-    res.status(200).json({ message: 'Users added to group', users: existingUsers });
+
+    if (existingUsersAggregation.length !== usersToAdd.length) {
+      return res.status(400).json({ error: 'One or more users do not exist' });
+    }
+
+    res.status(200).json({ message: 'Users added to group', users: existingUsersAggregation });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -42,13 +61,25 @@ const addUsersToGroup = async (req, res) => {
 
 const getUsersFromGroup = async (req, res) => {
   try {
-    const { groupId } = req.query;  
+    const { groupId } = req.query;
     if (groupId) {
-      const group = await Group.findById(groupId).populate('users');
-      if (!group) {
+      const groupAggregation = await Group.aggregate([
+        { $match: { _id: new mongoose.Types.ObjectId(groupId) } },
+        { $lookup: {
+            from: 'users',
+            localField: 'users',
+            foreignField: '_id',
+            as: 'users'
+          }
+        },
+        { $project: { _id: 0, users: 1 } }
+      ]);
+
+      if (groupAggregation.length === 0) {
         return res.status(404).json({ error: 'Group not found' });
       }
-      return res.status(200).json(group.users);
+
+      return res.status(200).json(groupAggregation[0].users);
     } else {
       const users = await User.find();
       return res.status(200).json(users);
@@ -57,9 +88,6 @@ const getUsersFromGroup = async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
-
-
-
 
 module.exports = {
   createGroup,
